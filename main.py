2f6b4f09dd4188ac23ca51da2b7c0793e20104c8
Arya <arya19@live.unc.edu>
2021-11-06 20:39:05 -0400

    Commit 4

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
 
+
+
+#tillage-how many km^2   limit: 1.8
+
+
+# NC only, choice 4 crops soybeans, peanuts, cotton, corn
+#if they allocate more land than they originally had, go back to beginning
+
+
+
+
+# seeding, 
+#how dense do you want the corn, soy beans, etc....
+#if they overseed reduce yield
+
+
+
+# application, 
+#1st watering
+#fertilizer
+#2 watering
+#pesticides
+#3 watering
+
+
+# harvest
+#return yield percentage 
+#return profit
+
+def main() -> None:
+    "Entrypoint into the program"
+
+
